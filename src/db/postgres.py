7d2b31a6b58acc4ca6b27ced76f7a938686a28f9
Renato Teixeira <rena2damas@gmail.com>
2021-10-03 17:09:34 +0300

    Create database

--- src/db/postgres.py
+++ src/db/postgres.py
@@ -1,25 +1,70 @@
+import os
+
+import click
+from psycopg2 import connect, extensions, sql
+
 from src.cli import dbutler
 
 
 @dbutler.group()
-def postgres():
+@click.option('-h', '--host')
+@click.option('-p', '--port')
+@click.option('-d', '--database')
+@click.option('-u', '--user')
+@click.option('-p', '--password')
+@click.pass_context
+def postgres(ctx, host, port, database, user, password):
     """Operations for a postgres database."""
-    pass
+    ctx.ensure_object(dict)
+    ctx.obj['host'] = host or os.getenv('PGHOST', 'localhost')
+    ctx.obj['port'] = port or os.getenv('PGPORT', 5432)
+    ctx.obj['database'] = database or os.getenv('PGDATABASE', 'postgres')
+    ctx.obj['user'] = user or os.getenv('PGUSER', 'postgres')
+    ctx.obj['password'] = password or os.getenv('PGPASSWORD')
 
 
 @postgres.group()
-def create():
+@click.pass_context
+def create(ctx):
     """Create postgres resources."""
-    pass
+    ctx.ensure_object(dict)
 
 
-@create.group()
-def database():
+@create.command(name='database')
+@click.argument('database')
+@click.pass_context
+def database_command(ctx, database):
     """Create postgres database."""
-    pass
+    ctx.ensure_object(dict)
+    exec_query(ctx, query=f"CREATE DATABASE {sql.Identifier(database)}")
 
 
-@create.group()
-def user():
+@create.command(name='user')
+def user_command():
     """Create postgres user."""
     pass
+
+
+def exec_query(ctx, query=''):
+    """Execute an postgres sql query."""
+    conn = connect(
+        host=ctx.obj['host'],
+        port=ctx.obj['port'],
+        database=ctx.obj['database'],
+        user=ctx.obj['user'],
+        password=ctx.obj['password']
+    )
+
+    # set the isolation level for autocommit
+    conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    # instantiate a cursor object from the connection
+    cursor = conn.cursor()
+
+    print(f"Executed query: {query}")
+    # use the sql module to avoid SQL injection attacks
+    cursor.execute(sql.SQL(query))
+
+
+    cursor.close()
+    conn.close()
